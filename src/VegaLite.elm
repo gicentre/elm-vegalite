673ce3941947f915b116cbdf4e41ba83b18a63b9
Jo Wood <jwo@soi.city.ac.uk>
2019-01-05 22:56:20 +0000

    More efficient and scalable transpose function

--- src/VegaLite.elm
+++ src/VegaLite.elm
@@ -11855,23 +11855,12 @@ yearMonthDateHoursMinutesSeconds =
 
 
 transpose : List (List a) -> List (List a)
-transpose ll =
-    case ll of
-        [] ->
-            []
-
-        [] :: xss ->
-            transpose xss
-
-        (x :: xs) :: xss ->
-            let
-                heads =
-                    List.filterMap List.head xss
-
-                tails =
-                    List.filterMap List.tail xss
-            in
-            (x :: heads) :: transpose (xs :: tails)
+transpose xss =
+    let
+        numCols =
+            List.head >> Maybe.withDefault [] >> List.length
+    in
+    List.foldr (List.map2 (::)) (List.repeat (numCols xss) []) xss
 
 
 
