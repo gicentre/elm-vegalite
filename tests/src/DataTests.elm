393eef4f3477cc69e11f2585acf534d5973f74e6
Jo Wood <jwo@soi.city.ac.uk>
2019-08-10 20:41:49 +0100

    Add biAnchor for anchoring a binning (VL3.4)

--- tests/src/DataTests.elm
+++ tests/src/DataTests.elm
@@ -427,6 +427,36 @@ bin1 =
     toVegaLite [ data [], enc [], bar [] ]
 
 
+bin2 : Spec
+bin2 =
+    let
+        data =
+            dataFromColumns []
+                << dataColumn "x" (nums [ 10.6, 12.1, 9.4, 11.5, 12.6, 10.7, 11.6, 7.7, 12, 10.6, 16.5, 8.7, 7.6, 10.2, 10, 9.8, 11, 9, 10.4, 11.6, 11.2, 11.1, 11.7, 12.1, 9.9, 8.9, 10.9, 14.6, 11.4, 12.1 ])
+
+        enc =
+            encoding
+                << position X [ pName "x", pMType Quantitative, pBin [] ]
+                << position Y [ pAggregate opCount, pMType Quantitative ]
+    in
+    toVegaLite [ width 300, data [], enc [], bar [] ]
+
+
+bin3 : Spec
+bin3 =
+    let
+        data =
+            dataFromColumns []
+                << dataColumn "x" (nums [ 10.6, 12.1, 9.4, 11.5, 12.6, 10.7, 11.6, 7.7, 12, 10.6, 16.5, 8.7, 7.6, 10.2, 10, 9.8, 11, 9, 10.4, 11.6, 11.2, 11.1, 11.7, 12.1, 9.9, 8.9, 10.9, 14.6, 11.4, 12.1, 12.2, 11.3, 13.1, 14.3, 9.8, 12.7, 9.2, 8.7, 11.3, 6.5, 11.1, 8.9, 11.8, 10.5, 12.8, 11.1, 11.2, 7, 12.4, 11.3, 8.3, 12.4, 12.1, 9.4, 8.6, 11.1, 8.9, 8.4, 10.5, 9.9, 6.5, 8.2, 12.7, 7.7, 11.1, 8.1, 8.1, 10.7, 9.8, 11.2, 11.2 ])
+
+        enc =
+            encoding
+                << position X [ pName "x", pMType Quantitative, pBin [ biAnchor 0.5 ] ]
+                << position Y [ pAggregate opCount, pMType Quantitative ]
+    in
+    toVegaLite [ width 300, data [], enc [], bar [] ]
+
+
 sequence1 : Spec
 sequence1 =
     let
@@ -572,6 +602,8 @@ mySpecs =
         , ( "impute8", impute8 )
         , ( "sample1", sample1 )
         , ( "bin1", bin1 )
+        , ( "bin2", bin2 )
+        , ( "bin3", bin3 )
         , ( "sequence1", sequence1 )
         , ( "sequence2", sequence2 )
         , ( "filter1", filter1 )
